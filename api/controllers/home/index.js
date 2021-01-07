7e188fe1780901fd1fe8646d329ee2f72027e216
gbs4ever2 <gbs4ever2@gmail.com>
2021-01-07 11:12:45 -0500

    first commit

---
+++ api/controllers/home/index.js
@@ -0,0 +1,26 @@
+module.exports = {
+
+
+  friendlyName: 'Index',
+
+
+  description: 'Index home.',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (_, exits) {
+    // All done.
+    exits.success({message: "Your Node API has successfully loaded"});
+  }
+
+
+};
